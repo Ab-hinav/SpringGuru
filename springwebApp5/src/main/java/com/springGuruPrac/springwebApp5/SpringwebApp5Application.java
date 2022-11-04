b5d6d9d685b5eff153ff243901af223ce0698fed
Abhinav Singh <abhinav.singh@mobisy.com>
2022-11-04 09:38:21 +0530

    proj setup

---
+++ springwebApp5/src/main/java/com/springGuruPrac/springwebApp5/SpringwebApp5Application.java
@@ -0,0 +1,13 @@
+package com.springGuruPrac.springwebApp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebApp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebApp5Application.class, args);
+	}
+
+}
---
+++ springwebApp5/src/test/java/com/springGuruPrac/springwebApp5/SpringwebApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springGuruPrac.springwebApp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
