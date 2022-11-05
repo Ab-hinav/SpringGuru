86b910d26a2e7865a380f0a4ade8f080283ef232
Abhi16DTU <abhi160197@gmail.com>
2022-11-05 15:15:43 +0530

    added petclinic

---
+++ mypetclinic1/src/main/java/com/mypetclinic/mypetclinic1/Mypetclinic1Application.java
@@ -0,0 +1,13 @@
+package com.mypetclinic.mypetclinic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mypetclinic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mypetclinic1Application.class, args);
+	}
+
+}
---
+++ mypetclinic1/src/test/java/com/mypetclinic/mypetclinic1/Mypetclinic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypetclinic.mypetclinic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mypetclinic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
