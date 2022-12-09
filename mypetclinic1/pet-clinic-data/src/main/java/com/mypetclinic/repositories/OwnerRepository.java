cc90262265aa61f87087f901de808e44b7dd5cdc
Abhi16DTU <abhi160197@gmail.com>
2022-12-09 22:48:30 +0530

    added jpa repos

---
+++ mypetclinic1/pet-clinic-data/src/main/java/com/mypetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.mypetclinic.repositories;
+
+import com.mypetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
